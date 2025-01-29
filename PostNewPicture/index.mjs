e5c2cdc7cbbc5457f9ed34c54661397dbd583b22
KhueDuong <duonghoangkhue.2004@gmail.com>
2025-01-29 15:26:34 +1300

    upload metadata to mongodb

--- PostNewPicture/index.mjs
+++ PostNewPicture/index.mjs
@@ -1,25 +1,52 @@
 import AWS from "aws-sdk";
+import { MongoClient } from "mongodb";
 
+const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
 const s3 = new AWS.S3();
 
 export const handler = async (event) => {
   try {
-    const fileName = event.filename;
+    // connect to mongodb
+    const db = await client.db("cloud-resume-data");
+    const collection = await db.collection("Pictures");
+
+    // get file data
+    const author = event.author;
+    const uploadDate = new Date();
+    const fileName = event.fileName;
     const fileContent = Buffer.from(event.fileContent, "base64");
 
+    // randomize file name to prevent duplication
+    const randomizedFileName =
+      fileName + "+" + Math.random().toString(36).slice(2);
+
+    // upload to s3
     const uploadParams = {
-      Bucket: "images-khuebanhzai.com", // Replace with your bucket name
-      Key: fileName, // The name of the file in S3
+      Bucket: "images-khuebanhzai.com",
+      Key: randomizedFileName,
       Body: fileContent,
       ContentType: "image/jpeg",
     };
+    const res = await s3.upload(uploadParams).promise();
 
-    await s3.upload(uploadParams).promise();
+    // upload metadata to db
+    const update = {
+      $set: {
+        author: author,
+        uploadDate: uploadDate,
+        fileName: fileName,
+        URL: res.Location,
+      },
+    };
+    const options = { upsert: true }; // Create the document if it doesn't exist
+
+    const result = await collection.updateOne(filter, update, options);
 
     return {
       statusCode: 200,
       body: JSON.stringify({
         message: "File uploaded successfully!",
+        res,
         fileName,
       }),
     };
